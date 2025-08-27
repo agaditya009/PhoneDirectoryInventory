1a0a635042fae8bf3eec7270b8ab5b18482a1d82
Aditya Gupta <51511425+agaditya009@users.noreply.github.com>
2025-08-27 19:07:01 +0530

    Ensure number activation is limited to allocated user

---
+++ src/test/java/com/assignment/phoneinventory/service/TelephoneServiceTest.java
@@ -0,0 +1,33 @@
+package com.assignment.phoneinventory.service;
+
+import com.assignment.phoneinventory.dao.TelephoneNumberDao;
+import com.assignment.phoneinventory.exception.BusinessRuleViolationException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class TelephoneServiceTest {
+
+    @Autowired
+    private TelephoneService service;
+
+    @Autowired
+    private TelephoneNumberDao dao;
+
+    @Test
+    void activatingWithDifferentUserFails() {
+        String number = "5551234";
+        dao.upsertNumber(number, "1", "2", "3");
+        service.reserve(number, "userA", Duration.ofMinutes(15));
+        service.allocate(number, "userA");
+
+        assertThrows(BusinessRuleViolationException.class,
+                () -> service.activate(number, "userB"));
+    }
+}
+
